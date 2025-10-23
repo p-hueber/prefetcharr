e85d42474ab141d5915b51f451acfdeea7970795
rotecode <31651906+rotecode@users.noreply.github.com>
2025-10-23 03:44:21 +0200

    feat(docker): allow running as non-root user in docker

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+set -e
 
-# Run in compat mode for the old config style
-test -z "$PREFETCHARR_CONFIG" && exec /prefetcharr \
+# Create config dir if it doesn't exist
+mkdir -p /config
+
+# Run in compat mode for the old config style, when no config file is present and PREFETCHARR_CONFIG is unset
+test ! -f /config/config.toml && test -z "${PREFETCHARR_CONFIG}" && exec /prefetcharr \
   --media-server-type "${MEDIA_SERVER_TYPE}" \
   --media-server-url "${MEDIA_SERVER_URL}" \
   --sonarr-url "${SONARR_URL}" \
@@ -12,9 +16,10 @@ test -z "$PREFETCHARR_CONFIG" && exec /prefetcharr \
   --libraries "${LIBRARIES:---libraries}" \
   --connection-retries 6 
 
-test -f /config || sh -c "cat > /config <<EOF
-$PREFETCHARR_CONFIG
-EOF"
+# $PREFETCHARR_CONFIG takes precedence over a pre-existing config file
+test -n "${PREFETCHARR_CONFIG}" && sh -c 'cat > /config/config.toml <<EOF
+${PREFETCHARR_CONFIG}
+EOF'
 
-exec /prefetcharr --config /config
+exec /prefetcharr --config /config/config.toml
 
